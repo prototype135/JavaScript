fb9814edd7041ba1b14b91d4e16e3deaf89b9d30
prototype135 <75358204+prototype135@users.noreply.github.com>
2020-12-18 12:12:13 +0900

    Add files via upload

---
+++ kokoa-clone/index.js
@@ -0,0 +1,35 @@
+const yeolInfo={
+    name:"Yeol",
+    age:27,
+    gender:"Male",
+
+    family:[
+        {
+            name:"June",
+            age:22,
+            gender:"Male"
+        },
+        {
+            name:"Mother",
+            age:54,
+            gender:"Female"
+        },
+        {
+            name:"Father",
+            age:57,
+            gender:"Male"
+        },
+        {
+            name:"Marvel",
+            age:2,
+            gender:"Male"
+        },
+        {
+            name:"Jerry",
+            age:11,
+            gender:"Male"
+        }
+    ]
+
+};
+console.log(yeolInfo,console);
