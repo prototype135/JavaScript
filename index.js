638b8a24bcd2a3efeb0e5a444aa03c855ebbbb45
prototype135 <75358204+prototype135@users.noreply.github.com>
2020-12-21 17:32:19 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const age = prompt("How old are you?");
+
+if(age >= 18 && age <=21){
+    console.log("hmm");
+} else if(age < 21){
+    console.log("too young");
+} else{
+    console.log("go ahead");
+}
